dc5ad4b68b53e959c856fbc6de6e46d4cc94b501
Nguyen Ninh Thuan <nninhthuan211@gmail.com>
2024-07-07 10:29:33 +0700

    the main page link to the search page

---
+++ src/assets/icon.js
@@ -0,0 +1,9 @@
+const Arrow = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-5.0 -10.0 110.0 135.0">
+      <path d="m81.75 49h-57l20.25-20.5-1.75-1.75-23.25 23.25 23.25 23.25 1.75-1.75-20-20h56.75z" />
+    </svg>
+  );
+}
+
+export default Arrow;
